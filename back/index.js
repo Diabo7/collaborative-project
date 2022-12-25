22cd76e95e0615632e82aa5fdfb74c9a0473ec93
Diabo7 <morgane.formentel7@gmail.com>
2022-12-25 14:43:34 +0100

    ajout liste de produits par catégorie

--- back/index.js
+++ back/index.js
@@ -9,22 +9,61 @@ app.use(
     origin: "*",
   })
 )
-const tab = [
-  {
-    image: "../assets/ordi1.jpg",
-    price: 1200,
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem."
-  },
-  {
-    image: "../assets/ordi2.jpg",
-    price: 500,
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem."
-  }]
+const categories = {
+  ordinateurs: [
+    {
+      image: "../assets/ordi1.jpg",
+      price: 1200,
+      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem."
+    },
+    {
+      image: "../assets/ordi2.jpg",
+      price: 500,
+      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo itaque laborum delectus accusantium non, nostrum ut inventore, earum tempore reprehenderit, nam quae unde dolore dolorem excepturi vero consectetur. Atque, exercitationem."
+    }
+  ],
+  composants: [
+    {
+      image: "../assets/ordi1.jpg",
+      price: 900,
+      description: "1"
+    },
+    {
+      image: "../assets/ordi2.jpg",
+      price: 700,
+      description: "2"
+    }
+  ],
+  peripheriques: [
+    {
+      image: "../assets/ordi1.jpg",
+      price: 400,
+      description: "3"
+    },
+    {
+      image: "../assets/ordi2.jpg",
+      price: 2000,
+      description: "4"
+    }
+  ],
+  gaming: [
+    {
+      image: "../assets/ordi1.jpg",
+      price: 10,
+      description: "5"
+    },
+    {
+      image: "../assets/ordi2.jpg",
+      price: 9000,
+      description: "6"
+    }
+  ]
+}
 
 app.get('/', (req, res) => {
   //res.send('Hello World!');
   //res.json({ name: "Kyle", favoriteFood: "Rice" });
-  res.send(tab)
+  res.send(categories)
 })
 
 /*app.get('/arg', (req, res) => {
--- front/accueil/script.js
+++ front/accueil/script.js
@@ -10,31 +10,35 @@ function httpGetAsync(theUrl, callback) {
     xmlHttp.send(null);
 }
 
-httpGetAsync("http://localhost:3005/", (e) => {
-    console.log("l'appel au back marche")
-    console.log(typeof e)
-    e = JSON.parse(e);
-    console.log(e)
+const url = new URL(document.URL);
+console.log(url);
+let type = url.searchParams.get("type");
+console.log(type);
 
-    for (let i = 0; i < e.length; i++) {
+httpGetAsync("http://localhost:3005/", (categories) => {
+    console.log("l'appel au back marche")
+    categories = JSON.parse(categories);
+    const elements = categories[type];
+    console.log(elements);
+
+    for (let i = 0; i < elements.length; i++) {
         let main = document.querySelector("main");
         let div = document.createElement("div");
         let table = document.createElement("table");
         const tr = table.insertRow();
         const tr_img = tr.insertCell();
         const img = document.createElement("img");
-        img.src = e[i]["image"];
+        img.src = elements[i]["image"];
         const tr_description = tr.insertCell();
         const description = document.createElement("p");
-        description.textContent = e[i]["description"];
+        description.textContent = elements[i]["description"];
         const tr_price = tr.insertCell();
         const price = document.createElement("p");
-        price.textContent = e[i]["price"];
+        price.textContent = elements[i]["price"];
         main.append(div);
         div.append(table);
         tr_img.append(img);
         tr_description.append(description);
         tr_price.append(price);
-
     }
 })
